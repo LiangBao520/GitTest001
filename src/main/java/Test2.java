15f1b1b257d5ebf17e445bd4595fb167ec4a8d11
‘LiangliangBao’ <2694625083@qq.com>
2022-07-30 17:22:48 +0800

    第二次提交

---
+++ src/main/java/Test2.java
@@ -0,0 +1,12 @@
+/**
+ * @Create: 2022-07-30 17:20
+ * @Author: liangliang
+ * @ToUser: Be happy every day! 😊
+ * ----------------------------------------
+ * @note:
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("第二次提交");
+    }
+}
