c21db113a0ffa215cbe74822107d6de609a6a77f
‘LiangliangBao’ <2694625083@qq.com>
2022-07-30 17:16:54 +0800

    第一次提交

---
+++ src/main/java/MyTest.java
@@ -0,0 +1,12 @@
+/**
+ * @Create: 2022-07-30 17:15
+ * @Author: liangliang
+ * @ToUser: Be happy every day! 😊
+ * ----------------------------------------
+ * @note:
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("你好世界！");
+    }
+}
